c1cbc429034798b7cac840d797037708b08c6b5d
meis <stephan.meier@nis.ch>
2020-01-08 14:58:06 +0100

    make single instance

--- bin/templates/project/cdv-electron-main.js
+++ bin/templates/project/cdv-electron-main.js
@@ -63,6 +63,19 @@ function createWindow () {
     });
 }
 
+var shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore();
+        mainWindow.focus();
+    }
+});
+
+if (shouldQuit) {
+    app.quit();
+    return;
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
